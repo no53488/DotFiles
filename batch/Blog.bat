d6d02ae472d14e9b6175036c6f01d486e35de3a8
no53488 <72663746+no53488@users.noreply.github.com>
2021-09-03 13:56:56 -0600

    Add files via upload

---
+++ batch/Blog.bat
@@ -0,0 +1,2 @@
+@echo off
+start chrome https://portal.spsd.sk.ca/blog/cp4luxw/Blog/default.aspx#/
---
+++ batch/JapaneseStart.bat
@@ -0,0 +1,7 @@
+call M:\Japanese\anime-manga.bat
+call M:\Japanese\KidsWeb.bat
+call M:\Japanese\ManyThings.bat
+call M:\Japanese\Proficiency.bat
+call M:\Japanese\Self-study.bat
+call M:\Japanese\Usagi-chan.bat
+pause
