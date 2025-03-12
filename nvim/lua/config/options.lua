4b5674ad25ccb2391e872224d6a97d99e18faecb
Nathan Olson <nathan.olson@ucalgary.ca>
2025-03-12 14:50:43 -0600

    March 2025

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,5 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- " This is necessary for VimTeX to load properly. The "indent" is optional.
+--Note: Most plugin managers will do this automatically!
