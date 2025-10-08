60e7da8798c890071aff26af85f690e4c79734b6
no53488 <nathanieljay216@gmail.com>
2025-10-08 16:50:07 -0600

    update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,10 @@
 -- Add any additional options here
 -- " This is necessary for VimTeX to load properly. The "indent" is optional.
 --Note: Most plugin managers will do this automatically!
+
+-- Set indents
+vim.cmd([[
+    filetype plugin indent on
+    set shiftwidth=4 smarttab
+    set expandtab
+]])
